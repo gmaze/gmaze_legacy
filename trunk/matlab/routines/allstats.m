70c2a503c559cd43c8d2a9b3714eade800411de5
maze.guillaume <maze.guillaume@77d81af0-8a4a-11dd-abc6-8b02bc135b05>
2008-09-25 21:32:01 +0000

    Eg for Taylordiag.m

---
+++ trunk/matlab/routines/allstats.m
@@ -0,0 +1,56 @@
+% STATM = allstats(Cr,Cf)
+%
+% Compute statistics from 2 series.
+% Cr is considered as the reference 
+% 
+% STATM(1,:) => Mean
+% STATM(2,:) => Standard Deviation (scale by N)
+% STATM(3,:) => Root Mean Square Difference
+% STATM(4,:) => Correlation
+%
+% Copyright (c) 2008 Guillaume Maze. 
+
+
+% This file is part of "The-Matlab-Show"
+% The-Matlab-Show is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% any later version.
+% Foobar is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% You should have received a copy of the GNU General Public License
+% along with The-Matlab-Show.  If not, see <http://www.gnu.org/licenses/>.
+%
+
+function STATM = allstats(varargin)
+	
+Cr = varargin{1};
+Cf = varargin{2};
+
+N = length(Cr);
+
+%%% STD:
+st(1) = sqrt(sum(  (Cr-mean(Cr) ).^2)  / N );
+st(2) = sqrt(sum(  (Cf-mean(Cf) ).^2)  / N );
+
+%%% MEAN:
+me(1) = mean(Cr);
+me(2) = mean(Cf);
+
+%%% RMSD:
+rms(1) = sqrt(sum(  ( ( Cr-mean(Cr) )-( Cr-mean(Cr) )).^2)  /N);
+rms(2) = sqrt(sum(  ( ( Cf-mean(Cf) )-( Cr-mean(Cr) )).^2)  /N);
+
+%%% CORRELATIONS:
+co(1) = sum(  ( ( Cr-mean(Cr) ).*( Cr-mean(Cr) )))/N/st(1)/st(1);
+co(2) = sum(  ( ( Cf-mean(Cf) ).*( Cr-mean(Cr) )))/N/st(2)/st(1);
+
+
+%%% OUTPUT
+STATM(1,:) = me;
+STATM(2,:) = st;
+STATM(3,:) = rms;
+STATM(4,:) = co;
+	
