50f2c3684387151658ae09685cd3c07ded07ff1d
maze.guillaume <maze.guillaume@77d81af0-8a4a-11dd-abc6-8b02bc135b05>
2010-04-03 13:13:21 +0000

    Matlab routines update

--- matlab/routines/lanfilt.m
+++ matlab/routines/lanfilt.m
@@ -1,39 +1,78 @@
 % LANFILT High, low, band pass filters based on Lanczos filter
 %
-% Y = LANFILT(X,fc_dw,fc_up,NJ,Filter)
-% X(1,:)
-% Filter= 1 : Low-pass (cutoff fc_up)
-%         2 : High-pass (cutoff fc_dw)
-%         3 : Band-pass (fc_dw<->fc_up)
-% Rq: X is detrended
+% Y = LANFILT(X,FILTER_TYPE,HFc,LFc,N)
+% 
+% Filter 1D signal X through a Lanczos filter.
 %
-% Copyright (c) 2004 Guillaume Maze. 
+% Inputs:
+%	X: Input signal (1 dimension of length n)
+%
+%	FILTER_TYPE: Type of filtering you want:
+%			1 : Low-pass  (cutoff frequency is LFc)
+%			2 : High-pass (cutoff frequency is HFc)
+%			3 : Band-pass (retain signal frequency within HFc<->LFc)
+%
+%	HFc: High frequency cut off (any k/n between 1/n and 1)
+%	LFc: Low frequency cut off (any k/n between 1/n and 1)
+%
+%	N: Number of points in the filter. Note that the transition width 
+%		of the transfer function is 2/(N-1)
+%
+% Outputs:
+%	Y: Filtered signal
+%
+% Copyright (c) 2004-2010 Guillaume Maze. 
+% Rev. by Guillaume Maze on 2010-02-18: Improved help, changed arguments order, add arguments check
 % http://codes.guillaumemaze.org
 
+% Copyright (c) 2010, Guillaume Maze (Laboratoire de Physique des Oceans).
+% All rights reserved.
+
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+% 	* Redistributions of source code must retain the above copyright notice, this list of 
+% 	conditions and the following disclaimer.
+% 	* Redistributions in binary form must reproduce the above copyright notice, this list 
+% 	of conditions and the following disclaimer in the documentation and/or other materials 
+% 	provided with the distribution.
+% 	* Neither the name of the Laboratoire de Physique des Oceans nor the names of its contributors may be used 
+%	to endorse or promote products derived from this software without specific prior 
+%	written permission.
 %
-% This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or any later version.
-% This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-% You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% THIS SOFTWARE IS PROVIDED BY Guillaume Maze ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+% INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+% PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Guillaume Maze BE LIABLE FOR ANY 
+% DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+% LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+% BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+% STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 %
 
-function [XF] = lanfilt(X,Fc1,Fc2,NJ,FILTER)
+function [XF] = lanfilt(X,FILTER,Fc1,Fc2,N)
+
+if FILTER < 0 | FILTER > 3
+	error('FILTER must be 1, 2 or 3 !');
+end
+
+X  = X(:);
+n  = length(X);
+NJ = fix((N-1)/2);
 
 
-X = detrend(X);
 switch FILTER
+	case 1 % LOW PASS
+	      XF = lanczos(X,Fc2,NJ);
 
-case 1 % LOW PASS
-      XF = lanczos(X',Fc2,NJ);
+	case 2 % HIGH PASS
+	      XF = X - lanczos(X,Fc1,NJ);
 
-case 2 % HIGH PASS
-      XF = X - lanczos(X',Fc1,NJ);
-
-case 3 % BAND PASS
-      XF = lanczos(X',Fc2,NJ);
-      XF = XF - lanczos(XF,Fc1,NJ);
+	case 3 % BAND PASS
+	      XF = lanczos(X,Fc2,NJ);
+	      XF = XF - lanczos(XF,Fc1,NJ);
 
 end %switch
-XF = detrend(XF)';
 
+
+end%function
--- matlab/routines/mailsend.m
+++ matlab/routines/mailsend.m
@@ -20,35 +20,36 @@
 
 function varargout = mailsend(email,subject,body)
 
-
-  fid = fopen('./.matlab_email.txt','w');
-    if ~iscell(body)
+fid = fopen('./.matlab_email.txt','w');
+if ~iscell(body)
 	% Check if we have multiple lines
 	ll=findstr(body,'\n');
 
 	if ~isempty(ll)
-	  body = ['\n' body '\n'];
-  	  ll = findstr(body,'\n');
-	  for il = 1 : length(ll)-1
-	    fprintf(fid,'%s\n',body(ll(il)+2:ll(il+1)-1));
-	  end
+		body = ['\n' body '\n'];
+		ll = findstr(body,'\n');
+		for il = 1 : length(ll)-1
+			fprintf(fid,'%s\n',body(ll(il)+2:ll(il+1)-1));
+		end
 	else
-          fprintf(fid,'%s',body);
+		fprintf(fid,'%s',body);
 	end
-	
-    else
-      for il = 1 : length(body)
-	fprintf(fid,'%s',cell2mat(body(il)));
-      end	
-    end
-  fclose(fid);
-  
-  chaine = sprintf('mail -s ''%s'' %s < %s',...
-		   subject,email,'./.matlab_email.txt');
-  system(chaine);
-  
-  delete('./.matlab_email.txt');
+
+else
+
+	for il = 1 : length(body)
+		fprintf(fid,'%s',cell2mat(body(il)));
+	end	
+
+end
+fclose(fid);
+
+chaine = sprintf('mail -s ''%s'' %s < %s',subject,email,'./.matlab_email.txt')
+unix(chaine);
+
+delete('./.matlab_email.txt');
 
   
+end%function
 
 
