a3b17bef448060ba3c9077c9df2f1d7a9bb5c629
maze.guillaume@gmail.com <maze.guillaume@gmail.com@77d81af0-8a4a-11dd-abc6-8b02bc135b05>
2012-12-20 14:48:22 +0000

    Update to Matlab codes (codes/matrix)

--- trunk/matlab/codes/matrix/Contents.m
+++ trunk/matlab/codes/matrix/Contents.m
@@ -1,22 +1,25 @@
-% MY CODES: Tools for data/matrix operations
-% Version 1 1 04-Mar-2011
+% MY CODES: MATRIX
+% Version 1 1 20-Dec-2012
 % 
 % 		Contents from /Users/gmaze/matlab/codes/matrix
 % 
-% Last update: 2011 March 04, 18:36
+% Last update: 2012 December 20, 15:17
 % 
 % 	assym                                    - Compute sym. or assym. component of 2D field
 % 	curvearea                                - Compute curve area in a crude way
+% 	datenumserie                             - Create a time serie with datenum
 % 	dim                                      - Give the number of dimensions of a field
 % 	dispt                                    - Display a table on prompt
 % 	duplicate                                - Find duplicate values among a 1D table
 % 	findp                                    - Find the p power of 10 of a number
 % 	iseven                                   - True for even array
+% 	isin                                     - Check if a table contains values of another table
 % 	m2cols                                   - Transform a 1 dimensional matrix to a square one with elements on columns
 % 	m2diag                                   - Transform a 1 dimensional matrix to a square one with elements on the diagonal
 % 	monthlabs                                - Get various labels of months
 % 	mywkeep                                  - Keep part of a vector or a matrix.
 % 	nansummap                                - Multidimensional nansum
+% 	rgb2web                                  - Convert an rgb colormap map to a javascript array of webcolor strings.
 % 	rotatepolygon                            - H1LINE
 % 	smoother1Ddiff                           - Apply a diffusive smoother on a 1D field
 % 	smoother2Ddiff                           - Apply a diffusive smoother on a 2D field
