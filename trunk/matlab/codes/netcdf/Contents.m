0e84ebb7603bf69539c8315d7fb37b57c1549eac
maze.guillaume@gmail.com <maze.guillaume@gmail.com@77d81af0-8a4a-11dd-abc6-8b02bc135b05>
2012-12-20 14:48:36 +0000

    Update to Matlab codes (codes/netcdf)

--- trunk/matlab/codes/netcdf/Contents.m
+++ trunk/matlab/codes/netcdf/Contents.m
@@ -1,10 +1,11 @@
-% MY CODES: Tools for NETCDF
-% Version 1 1 04-Mar-2011
+% MY CODES: NETCDF
+% Version 1 1 20-Dec-2012
 % 
 % 		Contents from /Users/gmaze/matlab/codes/netcdf
 % 
-% Last update: 2011 March 04, 18:36
+% Last update: 2012 December 20, 15:17
 % 
+% 	ncbuiltin                                - Is this Matlab using built-in netcdf or not ?
 % 	ncdimname                                - List dimensions name of a netcdf object
 % 	ncvardesc                                - List informations about variables of a netcdf object
 % 	ncvarlongname                            - List variables long name of a netcdf object
